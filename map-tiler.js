ba9f39c749b6a6cdc3e70f62d5a4b4e2f8702031
Val Laf <valerie.lafaiteur@gmail.com>
2021-12-29 08:44:15 +0100

    Structure html + css

---
+++ map-tiler.js
@@ -0,0 +1,9 @@
+// var urlParams = new URLSearchParams(window.location.search);
+// var key = urlParams.get('key');
+//
+// var map = new maplibregl.Map({
+//     container: 'map', // container id
+//     style: `https://api.maptiler.com/maps/streets/style.json?key=${key}`, // style URL
+//     center: [-74.5, 40], // starting position [lng, lat]
+//     zoom: 9 // starting zoom
+// });
